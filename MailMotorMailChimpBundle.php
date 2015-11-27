d734a0f215c4baf42ba4602cbbdb718ce236a0a2
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-11-27 10:35:36 +0100

    Adding DependencyInjection and Bundle init file

---
+++ MailMotorMailChimpBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MailMotor\Bundle\MailChimpBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * MailMotor MailChimp Bundle
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+class MailMotorMailChimpBundle extends Bundle
+{
+}
