14af5e707f05369eb4b5ce0af2fa52b7922a0e57
Jeroen Desloovere <jeroendesloovere@gmail.com>
2016-03-16 14:25:25 +0100

    Fixing Extension

--- DependencyInjection/MailMotorMailChimpExtension.php
+++ DependencyInjection/MailMotorMailChimpExtension.php
@@ -5,19 +5,19 @@ namespace MailMotor\Bundle\MailChimpBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class MailMotorMailChimpExtension extends ConfigurableExtension
+class MailMotorMailChimpExtension extends Extension
 {
     /**
      * {@inheritDoc}
      */
-    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    public function load(array $mergedConfig, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
